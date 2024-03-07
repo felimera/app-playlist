0650b0c62aadcdef62ff07ba100d48485e24a233
andres.mera <feli041089@gmail.com>
2024-03-07 08:24:05 -0500

    master : Creacion del  proyecto back.

---
+++ src/test/java/com/project/appplaylist/AppPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.appplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
